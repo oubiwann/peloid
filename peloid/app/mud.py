abf81b44e605599795aebc09601fd0e2f29b531d
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-13 16:45:39 -0700

    Added a placeholder for a default MUD game.

---
+++ peloid/app/mud.py
@@ -0,0 +1,8 @@
+class Game(object):
+    """
+    """
+    def start(self, *args, **kwargs):
+        pass
+
+    def parseCommand(self, *args,  **kwargs):
+        return {"args": args, "kwargs": kwargs}
