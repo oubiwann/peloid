e6484520b5fe6d3a136029bb38f0954ca211e831
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-27 01:11:30 -0700

    Fixed user registration code.

--- peloid/app/register.py
+++ peloid/app/register.py
@@ -4,6 +4,8 @@ from twisted.internet.defer import DeferredList, maybeDeferred
 from twisted.python import log
 from twisted.web.client import getPage
 
+from peloid.app.models.user import UserModel
+
 
 class Registration(object):
     """
@@ -12,23 +14,26 @@ class Registration(object):
         self.email = email
         self.sshKeysURL = sshKeysURL
 
-    def createUser(self):
-        return maybeDeferred(lambda: "success")
-
-    def processKeys(self, results):
-        if "launchpad.net" in self.sshKeysURL:
-            self._processLPKeys(results)
-        elif "github.com" in self.sshKeysURL:
-            self._processGitHubKeys(results)
+    def createUser(self, keys):
+        # XXX check to see if the user exists
+        # XXX if the user exists, raise an exception
+        user = UserModel(email=self.email, sshKeys=keys)
+        d = user.insert()
+        d.addErrback(log.err)
+        return d
 
     def _processGitHubKeys(self, results):
         data = json.loads(results)
-        for keyData in data:
-            log.msg(keyData.get("key"))
+        return [x.get("key") for x in data]
 
     def _processLPKeys(self, results):
-        for line in results.split("\n"):
-            log.msg(line)
+        return [x for x in results.split("\n")]
+
+    def processKeys(self, results):
+        if "launchpad.net" in self.sshKeysURL:
+            return self._processLPKeys(results)
+        elif "github.com" in self.sshKeysURL:
+            return self._processGitHubKeys(results)
 
     def getKeys(self):
         d = getPage(self.sshKeysURL)
@@ -36,13 +41,7 @@ class Registration(object):
         d.addErrback(log.err)
         return d
 
-    def saveUserKeys(self, results):
-        return maybeDeferred(lambda: "success")
-
     def run(self):
-        d = DeferredList([
-            self.getKeys(),
-            self.createUser()])
-        d.addCallback(self.saveUserKeys)
-        d.addErrback(log.err)
+        d = self.getKeys()
+        d.addCallback(self.createUser)
         return d
