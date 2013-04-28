bb97ccafe836a8bfb7b7c968843e85e25c03e371
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-27 21:44:07 -0700

    Started messing about with a command parser.

--- peloid/app/mud.py
+++ peloid/app/mud.py
@@ -1,8 +1,18 @@
+class World(object):
+    pass
+
+
+class Player(object):
+    pass
+
+
 class Game(object):
     """
     """
     def start(self, *args, **kwargs):
         pass
 
-    def parseCommand(self, *args):
-        return {"args": args}
+    def parseCommand(self, input):
+        parts = input.lower().split()
+        if parts[0] == "look":
+            return "You look around..."
