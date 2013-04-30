a414a3da857c341288eac9569135506860c04f64
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-29 22:02:09 -0700

    Added more room properties.
    Also, moved metagame rooms into module.

--- peloid/app/mud/room.py
+++ peloid/app/mud/room.py
@@ -6,10 +6,96 @@ from peloid import const
 class Room(object):
     """
     """
+    onEnterText = ""
+    onExitText = ""
+    exits = []
+
     def getDesc(self):
     	return self.__doc__
 
+    def getDescItems(self):
+        """
+        Describe the contents of the room.
+        """
+        # XXX do a database lookup for room contents, building a list of names
+        msg = "There are no items worth examining here."
+        return msg
 
+    def getDescPlayers(self):
+        """
+        Get a list of players in the room.
+        """
+        # XXX do a database lookup for players in the room
+
+    def getDescEnter(self):
+    	return self.onEnterText
+
+    def getDescExit(self):
+    	return self.onExitText
+
+    def getExitsText(self):
+        exits = ", ".join(self.exits[:-1])
+        return "This room has the following exits: %s, and %s." % (
+            exits, self.exits[-1])
+
+    def onEnter(self):
+    	pass
+
+    def onOpenDoor(self):
+    	pass
+
+    def onPassThreshold(self):
+    	pass
+
+    def onEnter(self):
+    	pass
+    	
+    def onExit(self):
+    	pass
+
+    def enter(self):
+    	# XXX write getDescEnter to the user's shell
+    	self.onOpenDoor()
+    	self.onPassThreshold()
+    	self.onEnter()
+
+    def exit(self):
+    	# XXX write getDescExit to the user's shell
+    	self.onOpenDoor()
+    	self.onPassThreshold()
+    	self.onExit()
+
+    
 class Hall(Room):
+	"""     
 	"""
-	"""
+
+
+class HallOfHalls(Hall):
+    """
+    """
+
+
+class HallOfCreators(Hall):
+    """
+    """
+
+
+class HallOfControllers(Hall):
+    """
+    """
+
+
+class HallOfAvatars(Hall):
+    """
+    """
+
+
+class HallOfViewing(Hall):
+    """
+    """
+
+
+class HallOfBanality(Hall):
+    """
+    """
--- peloid/app/mud/tests/test_room.py
+++ peloid/app/mud/tests/test_room.py
@@ -10,6 +10,24 @@ class TestRoom(room.Room):
 	suspended dust, and smells of deep must. Whether or not it was intended
 	originally, you can draw only one conclusion: this is now a test room.
 	"""
+	onEnterText = "A creaking door closes behind you."
+	onExitText = "You breath deeply; the test room has passed."
+	exits = ["W", "N", "E"]
+
+	def __init__(self):
+		self.testState = []
+
+	def onOpenDoor(self):
+		self.testState.append("door opened")
+
+	def onPassThreshold(self):
+		self.testState.append("passed through door")
+
+	def onEnter(self):
+		self.testState.append("entered room")
+
+	def onExit(self):
+		self.testState.append("exited room")
 
 
 class RoomTestCase(unittest.TestCase):
@@ -22,3 +40,30 @@ class RoomTestCase(unittest.TestCase):
 		self.assertTrue(self.room.getDesc().startswith("\n\t"))
 		self.assertTrue(self.room.getDesc().endswith("\n\t"))
 		self.assertEqual(len(self.room.getDesc()), 293)
+
+	def test_getDescEnter(self):
+		self.assertEqual(
+			self.room.getDescEnter(),
+			"A creaking door closes behind you.")
+
+	def test_getDescExit(self):
+		self.assertEqual(
+			self.room.getDescExit(),
+			"You breath deeply; the test room has passed.")
+
+	def test_getExitsText(self):
+		self.assertEqual(
+			self.room.getExitsText(),
+			"This room has the following exits: W, N, and E.")
+
+	def test_enter(self):
+		self.room.enter()
+		self.assertEqual(
+			self.room.testState,
+			['door opened', 'passed through door', 'entered room'])
+
+	def test_exit(self):
+		self.room.exit()
+		self.assertEqual(
+			self.room.testState,
+			['door opened', 'passed through door', 'exited room'])
